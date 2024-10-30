70ebb6015b0a224ca3a2279f61bc65a5dce66da8
Jessica W <102379487+jkwarren@users.noreply.github.com>
2024-10-30 15:14:07 -0400

    plays the uploaded audio file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,33 @@
 #include "mainwindow.h"
-
+#include <QtWidgets>
+#include <QAudioOutput>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    //widgets
+    QWidget *center = new QWidget();
+    setCentralWidget(center);
+    mainLayout = new QHBoxLayout(center);
+    uploadAudioButton = new QPushButton("upload");
+    mainLayout-> addWidget(uploadAudioButton, 0, Qt::AlignCenter);
+    connect(uploadAudioButton, &QPushButton::clicked, this, &MainWindow::uploadAudio);
+
+
+}
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::uploadAudio(){
+    QUrl aName = QFileDialog::getOpenFileUrl(this, "select audio file");
+    if (aName.isEmpty()) return;
+
+    player = new QMediaPlayer;
+    audioOutput = new QAudioOutput;
+    player->setAudioOutput(audioOutput);
+    // ...
+    player->setSource(aName);
+    audioOutput->setVolume(50);
+    player->play();
+
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,24 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QMediaPlayer>
+#include <QAudioOutput>
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    QHBoxLayout *mainLayout;
+    QPushButton *uploadAudioButton;
+    QMediaPlayer *player;
+    QAudioOutput *audioOutput;
+
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void uploadAudio();
 };
 #endif // MAINWINDOW_H
